60382118b98bd402cda66cff22cca1d5ab737765
Manoj Daswani <ManojDaswani11@gmail.com>
2020-11-06 09:16:51 +0530

    Initial Commit

---
+++ ObjectiveCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCDemo
+//
+//  Created by Manoj Daswani on 6/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
